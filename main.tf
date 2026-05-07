fc881afc45ed2db3cb963818ab308ed22ede843b
CharissaYeong <DonutDandy@gmail.com>
2026-05-07 20:16:38 +0800

    added terraform providers

--- main.tf
+++ main.tf
@@ -3,6 +3,17 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.5.0" 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" 
+    }
+  }
+  }
+
+terraform {
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket"
     key    = "3.2/terraform.tfstate"
