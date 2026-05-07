979ad727570770df0260cac7a8b398e5c1813cb4
CharissaYeong <DonutDandy@gmail.com>
2026-05-07 20:27:09 +0800

    changed s3-bucket resource

--- main.tf
+++ main.tf
@@ -24,3 +24,41 @@ terraform {
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "charissa-"
 }
+
+resource "aws_s3_bucket_public_access_block" "s3_tf_pab" {
+  bucket                  = aws_s3_bucket.s3_tf.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "s3_tf_versioning" {
+  bucket = aws_s3_bucket.s3_tf.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_tf_encryption" {
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "s3_tf_lifecycle" {
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    id     = "expire-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
